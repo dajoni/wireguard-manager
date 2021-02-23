f8f63f4a918d413449f4b3ed57c8a6efcc6fee5d
Daniel Nielsen <priv@te>
2021-02-23 22:30:23 +0400

    Added validation for server section

--- configuration.py
+++ configuration.py
@@ -5,10 +5,17 @@ class YamlValidationError(Exception):
     pass
 
 
+class ServerConfiguration:
+    def __init__(self) -> None:
+        super().__init__()
+        self.address = None
+
+
 class Configuration:
     def __init__(self) -> None:
         super().__init__()
         self.output_directory = None
+        self.server = ServerConfiguration()
 
 
 def configuration_from_yaml_file(file_name) -> Configuration:
@@ -27,19 +34,44 @@ def validate_context_has_data(context, configuration):
         raise YamlValidationError("No configuration data. Empty file?")
 
 
-def validate_context_has_correct_script_section(context, configuration):
+def validate_context_has_correct_script_section(context, configuration: Configuration):
     if "script" not in context.keys():
-        raise YamlValidationError("No script section found")
+        raise YamlValidationError("No script section found in configuration file")
     script_section = context['script']
     if script_section is None:
         raise YamlValidationError("Script section is empty")
-    if "output_directory" not in script_section.keys():
-        raise YamlValidationError("Key script.output_directory not found")
-    else:
-        configuration.output_directory = script_section['output_directory']
+    validate_field_exists("script", "output_directory", script_section)
+    configuration.output_directory = script_section['output_directory']
+
+
+def validate_context_has_correct_server_section(context, configuration: Configuration):
+    if "server" not in context.keys():
+        raise YamlValidationError("No server section found in configuration file")
+    server_section = context['server']
+    if server_section is None:
+        raise YamlValidationError("Server section is empty")
+    if not isinstance(server_section, dict):
+        raise YamlValidationError("Server section is not a dictionary")
+    validate_field_exists("server", "address", server_section)
+    validate_field_exists("server", "listen_port", server_section)
+    validate_field_exists("server", "endpoint_ip", server_section)
+    validate_field_exists("server", "public_key", server_section)
+    validate_field_exists("server", "private_key", server_section)
+    configuration.server.address = server_section['address']
+    configuration.server.listen_port = server_section['listen_port']
+    configuration.server.endpoint_ip = server_section['endpoint_ip']
+    configuration.server.public_key = server_section['public_key']
+    configuration.server.private_key = server_section['private_key']
+
+
+def validate_field_exists(section_name, field_name, section):
+    if field_name not in section.keys():
+        raise YamlValidationError(f"{section_name}.{field_name} not found in configuration file")
+
 
 def validate_yaml_and_build_configuration(context) -> Configuration:
     configuration = Configuration()
     validate_context_has_data(context, configuration)
     validate_context_has_correct_script_section(context, configuration)
+    validate_context_has_correct_server_section(context, configuration)
     return configuration
--- test_configuration.py
+++ test_configuration.py
@@ -37,7 +37,7 @@ foo:
     """
     context = configuration.yaml_from_stream(yaml_contents)
     assert context
-    with pytest.raises(configuration.YamlValidationError, match="No script section found"):
+    with pytest.raises(configuration.YamlValidationError, match="No script section found in configuration file"):
         configuration.validate_context_has_correct_script_section(context, empty_configuration)
 
 
@@ -58,7 +58,8 @@ script:
     """
     context = configuration.yaml_from_stream(yaml_contents)
     assert context
-    with pytest.raises(configuration.YamlValidationError, match="Key script.output_directory not found"):
+    with pytest.raises(configuration.YamlValidationError,
+                       match="script.output_directory not found in configuration file"):
         configuration.validate_context_has_correct_script_section(context, empty_configuration)
 
 
@@ -71,3 +72,96 @@ script:
     assert context
     configuration.validate_context_has_correct_script_section(context, empty_configuration)
     assert empty_configuration.output_directory == "foobar"
+
+
+def test_validate_context_has_correct_server_section_no_server_section(empty_configuration):
+    yaml_contents = """
+    foo:
+      bar
+        """
+    context = configuration.yaml_from_stream(yaml_contents)
+    assert context
+    with pytest.raises(configuration.YamlValidationError, match="No server section found in configuration file"):
+        configuration.validate_context_has_correct_server_section(context, empty_configuration)
+
+
+def test_validate_context_has_correct_server_section_empty_server_section(empty_configuration):
+    yaml_contents = """
+    server:
+        """
+    context = configuration.yaml_from_stream(yaml_contents)
+    assert context
+    with pytest.raises(configuration.YamlValidationError, match="Server section is empty"):
+        configuration.validate_context_has_correct_server_section(context, empty_configuration)
+
+
+def test_validate_context_has_correct_server_section_server_section_not_a_dict(empty_configuration):
+    yaml_contents = """
+    server:
+        foo
+        """
+    context = configuration.yaml_from_stream(yaml_contents)
+    assert context
+    with pytest.raises(configuration.YamlValidationError, match="Server section is not a dictionary"):
+        configuration.validate_context_has_correct_server_section(context, empty_configuration)
+
+
+def test_validate_context_has_correct_server_section_contains_address(empty_configuration):
+    yaml_contents = """
+    server:
+        foo: bar
+        """
+    context = configuration.yaml_from_stream(yaml_contents)
+    assert context
+    with pytest.raises(configuration.YamlValidationError, match="server.address not found in configuration file"):
+        configuration.validate_context_has_correct_server_section(context, empty_configuration)
+
+
+def test_validate_context_has_correct_server_section_contains_listen_port(empty_configuration):
+    yaml_contents = """
+    server:
+        address: bar
+        """
+    context = configuration.yaml_from_stream(yaml_contents)
+    assert context
+    with pytest.raises(configuration.YamlValidationError, match="server.listen_port not found in configuration file"):
+        configuration.validate_context_has_correct_server_section(context, empty_configuration)
+
+
+server_parameters = ["address", "listen_port", "endpoint_ip", "public_key", "private_key"]
+
+
+@pytest.mark.parametrize("field_name", server_parameters)
+def test_validate_context_has_correct_server_section_contains_field(field_name, empty_configuration):
+    yaml_contents = """
+    server:
+      address: 10.0.0.1/24
+      listen_port: 51820
+      endpoint_ip: 127.0.0.1
+      public_key: somepublickey=
+      private_key: someprivatekey=
+        """
+    context = configuration.yaml_from_stream(yaml_contents)
+    assert context
+    del context['server'][field_name]
+    with pytest.raises(configuration.YamlValidationError, match=f"server.{field_name} not found in configuration file"):
+        configuration.validate_context_has_correct_server_section(context, empty_configuration)
+
+
+def test_validate_context_has_correct_server_section(empty_configuration):
+    yaml_contents = """
+    server:
+      address: 10.0.0.1/24
+      listen_port: 51820
+      endpoint_ip: 127.0.0.1
+      public_key: somepublickey=
+      private_key: someprivatekey=
+        """
+    context = configuration.yaml_from_stream(yaml_contents)
+    assert context
+    configuration.validate_context_has_correct_server_section(context, empty_configuration)
+    assert empty_configuration.server.address == "10.0.0.1/24"
+    assert empty_configuration.server.listen_port == 51820
+    assert empty_configuration.server.endpoint_ip == "127.0.0.1"
+    assert empty_configuration.server.public_key == "somepublickey="
+    assert empty_configuration.server.private_key == "someprivatekey="
